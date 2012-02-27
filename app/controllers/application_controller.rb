f53ac5d6207bdcd96c3df41d75796c4a6a11c6cd
Doug Headley <headley.douglas@gmail.com>
2012-02-27 10:06:13 -0800

    added conditional to prevent edgecase where listing doesn't have a uuid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
   before_filter :current_office
   def current_office
     if params[:office_slug]
-      @office = WmsSvcConsumer::Models::Office.find_by_slug(params[:office_slug])
+      @office = Office.find_by_slug(params[:office_slug])
     elsif params[:listingid]
-      uuid = WmsSvcConsumer::Models::Listing.find(params[:listingid]).office.uuid
-      @office = WmsSvcConsumer::Models::Office.find(uuid)
+      uuid = Listing.find(params[:listingid]).office.uuid
+      @office = Office.find(uuid) unless uuid.nil?
     else
-      @office ||=  WmsSvcConsumer::Models::Office.find(8008563)
+      @office =  Office.find(8008563)
     end
   end
 end
